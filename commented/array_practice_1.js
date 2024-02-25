ee90f60ec8fe58d3afdb0faf1f212b5c48d1e18a
driyo <debashisr433@gmail.com>
2024-02-25 12:30:42 +0530

    Create array_practice_1.js

---
+++ commented/array_practice_1.js
@@ -0,0 +1,31 @@
+
+/*********array*********/
+
+// defination :     iterable object which contains different type of variables.
+
+// syntax :     
+
+const garrage = ['supra' , 'nissan GTR' , 'jaguar'];         // here garrage is the array object and inside it.. supra, nissan etc etc are elements
+
+// note :
+
+console.log(typeof garrage);                                // output : object
+
+
+// accessing elements :       we can access elements through the indexes just like string.
+
+console.log(garrage[0]);                                    // output : supra
+
+
+// changing or adding value : 
+
+console.log(garrage);                                      // output : [ 'supra', 'nissan GTR', 'jaguar' ]
+garrage[0] = 'BMW M8';
+console.log(garrage);                                      // output : [ 'BMW M8', 'nissan GTR', 'jaguar' ]                                 
+
+// note : if any value is not present at given index then it will add that value to that index only :
+
+garrage[100] = 'pagani';
+console.log(garrage);                                      // output :  [ 'BMW M8', 'nissan GTR', 'jaguar', <97 empty items>, 'pagani' ]
+
+
